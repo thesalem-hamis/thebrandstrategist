53da2200883b74246dc1dc9888f0159f91fe401d
Salem Hamis <140173982+salemhe@users.noreply.github.com>
2026-08-22 03:54:09 +0100

    final changes

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -1,5 +1,6 @@
 import { motion } from "framer-motion";
 import { ArrowUpRight } from "lucide-react";
+// import heroImage from "@/assets/main.png";
 import heroImage from "@/assets/main.png";
 
 export function Hero() {
